5593135d3098cb29159c86daaa800c9a040fdeab
Noahlp125 <noah84048@gmail.com>
2026-09-21 10:35:56 +0200

    test conexion vercel

--- src/app.jsx
+++ src/app.jsx
@@ -5,6 +5,7 @@ import Auth from './components/Auth'
 import Layout from './components/Layout'
 import Tasks from './components/Tasks'
 import Settings from './components/Settings'
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
   const [user, setUser] = useState(null)
